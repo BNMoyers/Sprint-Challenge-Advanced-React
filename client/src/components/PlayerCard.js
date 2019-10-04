1568e5a0031e39dcc5f1b519f53aead0d352dd2f
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-04 10:43:52 -0500

    player data mapped over and passed through PlayerList to PlayerCard

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlayerCard = ({ player }) => {
+    return(
+        <div>{player.name}</div>
+    )
+}
+
+export default PlayerCard
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PlayerCard from './PlayerCard'
 
-const PlayerList = () =>{
+const PlayerList = ({ players }) =>{
     return(
-        <div>
-            Player List
+        <div className = 'player-list'>
+            {players.map(player => <PlayerCard player={player} />)}
         </div>
     )
 }

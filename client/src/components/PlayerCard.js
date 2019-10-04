8a594ec67a126a1a12a57c3dc87057b7fd4652a9
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-10-04 11:29:08 -0500

    all info correctly displaying on styled cards

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+    card: {
+      minWidth: 275,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
 
 const PlayerCard = ({ player }) => {
+    const classes = useStyles();
+    
     return(
-        <div>{player.name}</div>
+        <Card className={classes.card}>
+        <CardContent>
+        <Typography className={classes.title} color='textSecondary' gutterBottom>
+        {player.searches}
+        </Typography>
+        <Typography variant="body2" component="p">
+                {player.name}
+            </Typography>
+        <Typography className={classes.pos} color='textSecondary'>
+            {player.country}
+            </Typography> 
+            
+             </CardContent></Card>
     )
 }
 
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import PlayerCard from './PlayerCard'
+import PlayerCard from './PlayerCard';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 
 const PlayerList = ({ players }) =>{
+    const classes = useStyles();
+
+        
+
     return(
-        <div className = 'player-list'>
-            {players.map(player => <PlayerCard player={player} />)}
+        
+        <div className = {classes.root}>
+            <Grid container spacing={1}>
+            {players.map(player => <Grid item xs={3}><Paper className={classes.paper}><PlayerCard player={player} /></Paper></Grid>)}
+            </Grid>
         </div>
-    )
+    );
 }
+
+
 
 export default PlayerList
